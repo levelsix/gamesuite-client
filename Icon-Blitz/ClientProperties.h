2f907011aa9e2d20b47e16b32ce647e7035884f8
Danny Huang <danny@lvl6.com>
2013-06-13 14:45:30 -0700

    finished tutorial

---
+++ Icon-Blitz/ClientProperties.h
@@ -0,0 +1,14 @@
+//
+//  ClientProperties.h
+//  Icon-Blitz
+//
+//  Created by Danny on 6/11/13.
+//
+//
+
+#ifndef Icon_Blitz_ClientProperties_h
+#define Icon_Blitz_ClientProperties_h
+
+
+
+#endif
---
+++ Icon-Blitz/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Icon-Blitz
+//
+//  Created by Danny on 6/11/13.
+//
+//
+
+#ifndef Icon_Blitz_Header_h
+#define Icon_Blitz_Header_h
+
+
+
+#endif
